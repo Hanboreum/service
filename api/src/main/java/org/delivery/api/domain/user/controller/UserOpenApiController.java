aa77889570067a1976b16ce8632ad55a1dd4793f
fullm <fullmoon0207@naver.com>
2024-05-21 23:33:14 +0900

    Fix: Swagger 에러 해결

--- api/src/main/java/org/delivery/api/domain/user/controller/UserOpenApiController.java
+++ api/src/main/java/org/delivery/api/domain/user/controller/UserOpenApiController.java
@@ -8,26 +8,23 @@ import org.delivery.api.domain.user.business.UserBusiness;
 import org.delivery.api.domain.user.controller.model.UserLoginRequest;
 import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
 import org.delivery.api.domain.user.controller.model.UserResponse;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.*;
 
 
 @RequiredArgsConstructor
-@RestControllerAdvice
+@RestController
 @RequestMapping("/open-api/user")
 public class UserOpenApiController {
 
     private final UserBusiness userBusiness;
 
-    //가입, userresponse 리턴해서
+    // 사용자 가입 요청
     @PostMapping("/register")
     public Api<UserResponse> register(
             @Valid
-            @RequestBody Api<UserRegisterRequest> request //1. req가 들어온다, valid검증 완료 후
+            @RequestBody Api<UserRegisterRequest> request
     ){
-        var response = userBusiness.register(request.getBody()); //2. 비즈스 로직에 전달
+        var response = userBusiness.register(request.getBody());
         return Api.OK(response);
     }
 
