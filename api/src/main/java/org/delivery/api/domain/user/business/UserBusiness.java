60adf39d54fa917a266de96c235a184fdd49c8ba
fullm <matoma1_@naver.com>
2024-05-21 18:19:20 +0900

    Feat: 사용자 서비스 로직 로그인

--- api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
+++ api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.delivery.api.common.annotation.Business;
 import org.delivery.api.common.error.ErrorCode;
 import org.delivery.api.common.exception.ApiException;
+import org.delivery.api.domain.user.controller.model.UserLoginRequest;
 import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
 import org.delivery.api.domain.user.controller.model.UserResponse;
 import org.delivery.api.domain.user.converter.UserConverter;
@@ -41,5 +42,22 @@ public class UserBusiness {
                 .map(userConverter::toResponse)
                 .orElseThrow(() ->new ApiException(ErrorCode.NULL_POINT,"Request null"));
     */
+    }
+
+    /**
+     * 1.email, password로 회원 체크
+     * 2. user entity 로그인 확인
+     * 3. 토근 확인
+     * 4. 토큰 response
+     * @param request
+     */
+    public UserResponse login(UserLoginRequest request) {
+
+        //사용자 없으면 throw
+        var userEntity = userService.login(request.getEmail(), request.getPassword());
+
+        //TODO 토큰 생성 로직으로 변경하기
+
+        return userConverter.toResponse(userEntity);
     }
 }
--- api/src/main/java/org/delivery/api/domain/user/service/UserService.java
+++ api/src/main/java/org/delivery/api/domain/user/service/UserService.java
@@ -2,6 +2,7 @@ package org.delivery.api.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.error.UserErrorCode;
 import org.delivery.api.common.exception.ApiException;
 import org.delivery.db.user.UserEntity;
 import org.delivery.db.user.UserRepository;
@@ -30,5 +31,21 @@ public class UserService {
                     return userRepository.save(userEntity); //save된 userentity를 비즈니스 로직으로
                 })
                 .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT,"User entity Null"));
+    }
+
+    public UserEntity login(String email,
+                            String password
+    ){
+        var entity = getUserWithThrow(email,password);
+        return  entity;
+    }
+
+    public UserEntity getUserWithThrow(String email, String password){
+
+        return userRepository.findFirstByEmailAndPasswordAndStatusOrderByIdDesc(
+                email,
+                password,
+                UserStatus.REGISTERED
+        ).orElseThrow(()-> new ApiException(UserErrorCode.USER_NOT_FOUND));
     }
 }
