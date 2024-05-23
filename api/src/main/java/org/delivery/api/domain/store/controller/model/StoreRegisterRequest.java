069104cd85299527490e2d69c0debf102209df7a
fullm <fullmoon0207@naver.com>
2024-05-23 15:40:35 +0900

    Feat: store 등록 조회 기능 완성

--- api/src/main/java/org/delivery/api/domain/store/controller/model/StoreRegisterRequest.java
+++ api/src/main/java/org/delivery/api/domain/store/controller/model/StoreRegisterRequest.java
@@ -1,5 +1,7 @@
 package org.delivery.api.domain.store.controller.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,18 +16,25 @@ import java.math.BigDecimal;
 public class StoreRegisterRequest {
     //등록요청
 
+    @NotBlank
     private String name;
 
+    @NotBlank
     private String address;
 
+    @NotNull //enum
     private StoreCategory category;
 
+    @NotBlank
     private String thumbnailUrl;
 
+    @NotNull
     private BigDecimal minimumAmount;
 
+    @NotNull
     private BigDecimal minimumDeliveryAmount;
 
+    @NotBlank
     private String phoneNumber;
 
 
