502d2723994c1b9ebf19b49ea887260d5bb075a2
fullm <fullmoon0207@naver.com>
2024-05-25 00:40:16 +0900

    Feat: UserOrder 서비스 로직

--- db/src/main/java/org/delivery/db/userordermenu/UserOrderMenuRepository.java
+++ db/src/main/java/org/delivery/db/userordermenu/UserOrderMenuRepository.java
@@ -1,6 +1,13 @@
 package org.delivery.db.userordermenu;
 
+import org.delivery.db.userordermenu.enums.UserOrderMenuStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserOrderMenuRepository extends JpaRepository<UserOrderMenuEntity, Long> {
+
+    //주문 아이디에 해당되는 등록된 모든 메뉴 리스트를 리턴
+    //select * user_order_menu where user_order_id =[] status=[]
+    List<UserOrderMenuEntity> findAllByUserOrderIdAndStatus(Long userOrderId, UserOrderMenuStatus status);
 }
