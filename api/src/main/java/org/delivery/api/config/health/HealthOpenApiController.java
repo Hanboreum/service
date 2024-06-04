81b09327a15ef707851070b735c8c259abccbd1d
fullm <fullmoon0207@naver.com>
2024-06-04 17:30:19 +0900

    Feat: rabbitmq, Producer 설정 테스트

---
+++ api/src/main/java/org/delivery/api/config/health/HealthOpenApiController.java
@@ -0,0 +1,23 @@
+package org.delivery.api.config.health;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.delivery.api.common.rabbitmq.Producer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/open-api")
+@Slf4j
+public class HealthOpenApiController {
+
+    private final Producer producer;
+
+    @GetMapping
+    public void health(){
+        log.info("health call");
+        producer.producer("delivery.exchange", "delivery.key", "hello");
+    }
+}
