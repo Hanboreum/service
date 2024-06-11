5d8c2ac3e25983acb23312a6ffda2a67da474825
fullm <fullmoon0207@naver.com>
2024-06-11 18:43:50 +0900

    Comment: 에러 설명 주석

--- api/src/main/java/org/delivery/api/common/error/ErrorCode.java
+++ api/src/main/java/org/delivery/api/common/error/ErrorCode.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-@Getter
+@Getter //override
 @AllArgsConstructor
 public enum ErrorCode implements ErrorCodeIfs{
 
