4f73dfcb1f8925e2cccca98729c62b0a1a43af4a
fullm <fullmoon0207@naver.com>
2024-06-14 12:24:38 +0900

    Comment: consumer 설정

--- store-admin/src/main/java/org/delivery/storeadmin/config/objectmapper/ObjectMapperConfig.java
+++ store-admin/src/main/java/org/delivery/storeadmin/config/objectmapper/ObjectMapperConfig.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//rabbitmq 설정 시 추가됨
 @Configuration
 public class ObjectMapperConfig {
 
--- store-admin/src/main/java/org/delivery/storeadmin/config/rabbitmq/RabbitMqConfig.java
+++ store-admin/src/main/java/org/delivery/storeadmin/config/rabbitmq/RabbitMqConfig.java
@@ -6,9 +6,11 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//rabbitmq 설정
 @Configuration
 public class RabbitMqConfig {
 
+    //yaml에 있는 설정을 사용해 connection factory가 만들어진다
     @Bean
     public MessageConverter messageConverter(ObjectMapper objectMapper){
         return new Jackson2JsonMessageConverter(objectMapper);
